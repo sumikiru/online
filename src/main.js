104dac34f8fd70f41f83ae233d8d8188c90cf8c9
sumikiru <230695489@qq.com>
2024-11-19 23:38:04 +0800

    框架更新
    完善了项目框架

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import './assets/main.css'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-const app = createApp(App)
-app.use(ElementPlus, { size: 'small', zIndex: 3000 })
-
 import { createApp } from 'vue'
+
+//整体导入 ElementPlus 组件库
+import ElementPlus from 'element-plus' //导入 ElementPlus 组件库的所有模块和功能
+import 'element-plus/dist/index.css' //导入 ElementPlus 组件库所需的全局 CSS 样式
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus) //将 ElementPlus 插件注册到 Vue 应用中
+app.mount('#app')
